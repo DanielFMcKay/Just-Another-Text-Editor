d9db4b75cacce7e77103165cdaaa4cf76656393a
Dan <123746582+DanielFMcKay@users.noreply.github.com>
2023-07-16 07:37:42 -0700

    Heroku, you suck

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,36 +1,23 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-const { StaleWhileRevalidate } = require('workbox-strategies');
-
-precacheAndRoute(self.__WB_MANIFEST);
-
-
-
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');precacheAndRoute(self.__WB_MANIFEST);// Set up page cache
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
-    // Cache pages for a maximum of 30 days
     new ExpirationPlugin({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
-});
-
-warmStrategyCache({
+});warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
-});
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// TODO: Implement asset caching
+});registerRoute(({ request }) => request.mode === 'navigate', pageCache);// Set up asset cache
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
@@ -38,10 +25,6 @@ registerRoute(
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
-      }),
-      // Cache pages for a maximum of 30 days
-      new ExpirationPlugin({
-        maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
     ],
   })
